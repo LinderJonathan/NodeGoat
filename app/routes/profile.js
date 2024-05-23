0077647c500f855b8d10b3ff6ef982064af5f1f8
LinderJonathan <linderjonathan3@gmail.com>
2024-05-23 23:21:28 +0200

    bullshit

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -22,21 +22,12 @@ function ProfileHandler(db) {
             if (err) return next(err);
             doc.userId = userId;
 
-            // @TODO @FIXME
-            // while the developer intentions were correct in encoding the user supplied input so it
-            // doesn't end up as an XSS attack, the context is incorrect as it is encoding the firstname for HTML
-            // while this same variable is also used in the context of a URL link element
-
+            doc.website = ESAPI.encoder().encodeForHTML(doc.website); // already in place by NodeGoat contributor
             /*
-            VULNERABLE POINTS: URL context not encoded
+            Mitigative XSS layer: encoding upon load of the page. Apart from OWASP's encoding of HTML
+            also encode for URL and JavaScript contexts.
             */
-            doc.website = ESAPI.encoder().encodeForHTML(doc.website);
             doc.website = ESAPI.encoder().encodeForJavaScript(doc.website);
-
-            /*
-            Mitigative XSS layer: also encode for URL contexts
-            See below for encoding
-            */
             doc.website = ESAPI.encoder().encodeForURL(doc.website);
 
             return res.render("profile", {
@@ -57,7 +48,6 @@ function ProfileHandler(db) {
             bankAcc,
             bankRouting
         } = req.body;
-        const stringFields = [firstName, lastName, ssn]
         // Fix for Section: ReDoS attack
         // The following regexPattern that is used to validate the bankRouting number is insecure and vulnerable to
         // catastrophic backtracking which means that specific type of input may cause it to consume all CPU resources
@@ -66,20 +56,22 @@ function ProfileHandler(db) {
         // The Fix: Instead of using greedy quantifiers the same regex will work if we omit the second quantifier +
         // const regexPattern = /([0-9]+)\#/;
 
-        // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#'
-        const regexPattern = /([0-9]+)+\#/;
-        const testComplyWithRequirements = regexPattern.test(bankRouting);
 
-        // strictly allows upper/lower alphabetical chars and '-', ' ' '
         /*
-        Mitigative XSS layer: Validating user input with Regular Expressions
+        Mitigative XSS layer: Validating user input with Regular Expressions on submitting/updating the profile
 
         By validating each input for each field according to regex patterns, bypassing with malicious attacks becomes
-        difficult. Here, NodeGoat already provided the regex pattern for 
+        difficult. Here, NodeGoat already provided the regex pattern for 'bankRouting'
         */
+        // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#' (by OWASP)
+        const regexPattern = /([0-9]+)+\#/; // already in place by NodeGoat contributor
+        const testComplyWithRequirements = regexPattern.test(bankRouting);      
+
+        // Allow only upper/lower alphabetical chars
         const regexTextField = /^[a-zA-Z\s-]+$/;
         const testFirstName = regexTextField.test(lastName);
         const testLastName = regexTextField.test(lastName);
+        
 
         function profileRender(errorResponse) {
             const firstNameSafeString = firstName;
@@ -95,13 +87,19 @@ function ProfileHandler(db) {
                 environmentalScripts
             });
         }
+
+        /*
+        Checks if user input violates the format
+        If it does, it calls for 'profileRender' which in turn sends an error message and
+        won't update the fields
+        */
         if (testFirstName !== true) {
             profileRender("Sorry, first names only contain lower/upper case letters and binders/apostrophes");
         }
-        else if (testLastName !== true) {
+        if (testLastName !== true) {
             profileRender("Sorry, last names contain lower/upper case letters and binders/apostrophes");       
         }
-        else if (testComplyWithRequirements !== true) {
+        if (testComplyWithRequirements !== true) {
             profileRender("Bank Routing number does not comply with requirements for format specified");
         }
 
@@ -110,12 +108,16 @@ function ProfileHandler(db) {
         } = req.session;
         
         /*
-        Mitigative XSS layer: Encode all user inputs
+        Mitigative XSS layer: Encode all user inputs with npm "sanitize-html"
+        Link: https://www.npmjs.com/package/sanitize-html
         
-        This sanitization works to prevent stored XSS. While this sanitization defaults to blocking
+        This is yet another sanitization layer that works to prevent a user from inputing malicious payload into
+        input fields on the profile page. While this sanitization defaults to blocking
         all HTML, it can be specified which tags or attributes should be allowed. For instance, a <b>bold</b> is mostly
         harmless, and is sometimes part of functionality. By creating a whitelist on tags and attributes, 
         they can bypass sanitization
+
+        Incredibly important that we sanitize BEFORE we update the user's profile
         */
 
 
